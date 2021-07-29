7e46e7d0dbb81d2346de5f0e8f33323daf911cfc
siewiera <ssiewiera29@gmail.com>
2021-07-29 22:13:27 +0200

    git init

--- mail.php
+++ mail.php
@@ -19,13 +19,13 @@ if(isset($_POST['submit'])){
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'ssiewiera29@gmail.com'; // Gmail address which you want to use as SMTP server
-    $mail->Password = 'LOrd123;09'; // Gmail address Password
+    $mail->Username = 'email'; // Gmail address which you want to use as SMTP server
+    $mail->Password = 'password'; // Gmail address Password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = '587';
+    $mail->Port = '587'; //Port for gmail
 
-    $mail->setFrom('ssiewiera29@gmail.com'); // Gmail address which you used as SMTP server
-    $mail->addAddress('ssiewiera29@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
+    $mail->setFrom('email'); // Gmail address which you used as SMTP server
+    $mail->addAddress('email'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
 
     $mail->isHTML(true);
     $mail->Subject = 'Message Received (Contact Page)';
